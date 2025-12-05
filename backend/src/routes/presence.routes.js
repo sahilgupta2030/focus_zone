3678f8cbf3c812ffa791d322fde84097c5a44fb7
Sahil Gupta <sahil20gupta30@gmail.com>
2025-12-05 21:07:02 +0530

    presence routes

--- backend/src/routes/presence.routes.js
+++ backend/src/routes/presence.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middleware/verifyJWT.middleware.js";
 
 import {
     setUserOnline,
